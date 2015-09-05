a5a35b0eda7563fa8fe1b11c84902ba2131aaa39
jharkins95 <harkj@seas.upenn.edu>
2015-09-05 19:28:26 -0400

    Controls.java

--- src/com/example/jthem/PennApps2015/Game.java
+++ src/com/example/jthem/PennApps2015/Game.java
@@ -1,9 +1,17 @@
 package com.example.jthem.PennApps2015;
 
+import edu.princeton.cs.introcs.*;
+
 public class Game {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		StdDraw.clear();
+		StdDraw.setCanvasSize(800, 600);
+		StdDraw.setXscale(0, 800);
+		StdDraw.setYscale(0, 600);
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.filledRectangle(250, 250, 25, 25);
 
 	}
 
