68eeef856da1f4b15e59e3d4daa6c7aed6263071
MauKMu <mkenzom@gmail.com>
2015-09-05 21:15:41 -0300

    classpath test

--- src/com/example/jthem/PennApps2015/Player.java
+++ src/com/example/jthem/PennApps2015/Player.java
@@ -6,9 +6,28 @@ import edu.princeton.cs.introcs.*;
 
 public class Player extends GameObject {
 
+    private int MOVE_INC = 10;
+    
+    
     public Player(int pX, int pY, int vX, int vY) {
         super(pX, pY, vX, vY, Tag.ALLY);
         // TODO Auto-generated constructor stub
+    }
+    
+    public void moveLeft() {
+        velX -= MOVE_INC;
+    }
+    
+    public void moveRight() {
+        velX += MOVE_INC;
+    }
+    
+    public void moveUp() {
+        velY += MOVE_INC;
+    }
+    
+    public void moveDown() {
+        velY -= MOVE_INC;
     }
     
     @Override
